ddc5bc1a46ad06e78bc87669f23dccf352eea43e
Lucas Serafim <lslucas@gmail.com>
2012-04-26 21:28:32 -0500

    melhorado form de login

--- login.php
+++ login.php
@@ -15,7 +15,7 @@
 	body { background-color:#d9d9d9; }
 </style>
 <center>
-	<form action="index.php" method="post" class='cmxform form-inline login' style='width:500px; text-align: left; '>
+	<form action="index.php" method="post" class='cmxform form-inline login' style='width:530px; text-align: left; '>
 	<div class='well'>
 			<img src='<?=SITE_URL?>/images/treinamentoeacao.png' width=170 border=0 style='float: left; margin-right:10px; margin-top: 40px;'/>
 			<!--<img src="http://placehold.it/160x180" alt="Logo" style='float: left; margin-right:20px;'>-->
@@ -24,14 +24,20 @@
 			<div class="control-group">
 			  <label class="control-label" for="username">Email</label>
 			  <div class="controls">
-				<input type="text" class="input-xlarge email required" placeholder='email@provedor.com.br' name='username' id='username'>
+				<div class="input-prepend">
+	                <span class="add-on"><i class='icon-user'></i></span><input type="text" class="input-xlarge email required" placeholder='email@provedor.com.br' name='username' id='username'>
+				</div>
 			  </div>
 			</div>
 
 			<div class="control-group">
 			  <label class="control-label" for="password">Senha</label>
 			  <div class="controls">
-				<input type="password" class="input-xlarge required" name='password' id='password'>
+				
+				<div class="input-prepend">
+	                <span class="add-on"><i class='icon-lock'></i></span><input type="password" class="input-xlarge required" placeholder='senha' name='password' id='password'>
+				</div>
+
 			  </div>
 			</div>
 
@@ -40,6 +46,8 @@
 				<input type='submit' value='Login' class='btn btn-primary'>
 				<a href='esqueci-senha.php' value='Esqueci minha senha' class='btn'>Esqueci a senha</a>
 			</div>
+
+			</fieldset>
 	</div>
 	</form>
 </center>
